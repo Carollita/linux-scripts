73f3ba6ca4316eb384a469571169d1a2ac8002d7
Carolina <carolsalgadoguadanhini@gmail.com>
2023-03-05 10:59:28 -0300

    comment stand-alone-service

--- docker-practical-use-in-microservices/shell-scripts/stand-alone-service.sh
+++ docker-practical-use-in-microservices/shell-scripts/stand-alone-service.sh
@@ -6,13 +6,14 @@ echo -e "###\nCreate volumes\n###"
 docker volume create app
 docker volume create data
 
-echo -e "###\nDownloading files\n###"
-cd /var/lib/docker/volumes/app/_data
-wget https://github.com/jpmjunior/dio-desafio-microservicos/archive/refs/heads/main.zip
-apt-get install unzip -y
-unzip main.zip
-mv -v dio-desafio-microservicos-main/* .
-rm -rv dio-desafio-microservicos-main main.zip
+# If you prefer, you can download and edit these files instead of creating
+# echo -e "###\nDownloading files\n###"
+# cd /var/lib/docker/volumes/app/_data
+# wget https://github.com/jpmjunior/dio-desafio-microservicos/archive/refs/heads/main.zip
+# apt-get install unzip -y
+# unzip main.zip
+# mv -v dio-desafio-microservicos-main/* .
+# rm -rv dio-desafio-microservicos-main main.zip
 
 echo -e "###\nCreating MySQL Container\n###"
 docker run -e MYSQL_ROOT_PASSWORD=pass123 -e MYSQL_DATABASE=mydata --name mysql-A -d -p 3306:3306 --mount type=volume,src=data,dst=/var/lib/mysql/ mysql:5.7
